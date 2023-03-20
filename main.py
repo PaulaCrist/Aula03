9675a5a9ff15b462a07ca2b4b52f2a8defc8fc96
PaulaCrist <128405171+PaulaCrist@users.noreply.github.com>
2023-03-20 13:21:58 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#questão 1
+print("eu amo programar em python")
+
+#questão 2
+nome = input("Qual é o seu nome? ")
+print("Bom dia, " + nome + "!")
+
+#questão 3
+numero = float(input("Qual o seu numero? "))
+resultado = ((numero + 1357) * 8 / 5) ** 2
+print(resultado)
+
+#questão 4
+name = input("qual o seu nome? ")
+print("----------------------")
+print("Convite para festa")
+print("----------------------")
+print("Você é nosso convidado")
+print("inauguração da Fênix")
+print("     ", name.upper())
+
+#questão 5
+print(4+4)
+print(10-2)
+print(2*4)
+print(16/2)
